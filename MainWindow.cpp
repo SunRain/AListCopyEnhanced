2ebe5d75b6e82646fafd44aebe5a67aed4a57508
SunRain <41245110@qq.com>
2024-12-03 00:03:15 +0800

    wip

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,28 @@
 #include "MainWindow.h"
 
 #include <QDebug>
+#include <QDateTime>
+#include <QTimeZone>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QJsonArray>
+#include <QJsonDocument>
 
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QRadioButton>
 
+#include <QNetworkRequest>
+#include <QNetworkReply>
+
+#include "PrivateConst.h"
+
+#define SET_PRIVATE_CONST(value, widgetPtr) \
+    do { \
+        if (!value.isEmpty()) { \
+            widgetPtr->setText(value); \
+        } \
+    } while (0); \
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,8 +34,15 @@ MainWindow::MainWindow(QWidget *parent)
     , m_copyBtn(new QPushButton)
     , m_showDiffBtn(new QPushButton)
     , m_logWidget(new QListWidget)
+    , m_networkMgr(new QNetworkAccessManager(this))
     , m_dupOpt(DuplicateOption::NoOverwrite)
+    , m_server(P_SERVER)
+    , m_token(P_TOKEN)
 {
+    SET_PRIVATE_CONST(P_SERVER,     m_serverEdt);
+    SET_PRIVATE_CONST(P_TOKEN,      m_tokenEdt);
+    SET_PRIVATE_CONST(P_SRC_DIR,    m_srcPathEdt);
+    SET_PRIVATE_CONST(P_DST_DIR,    m_dstPathEdt);
 
     auto vbox = new QVBoxLayout;
 
@@ -106,13 +130,7 @@ MainWindow::MainWindow(QWidget *parent)
         m_token = text;
     });
 
-    connect(m_showDiffBtn, &QPushButton::clicked,
-            this, [=] (bool checked) {
-        if (!chkServerAndToken()) {
-            return;
-        }
-
-    });
+    connect(m_showDiffBtn, &QPushButton::clicked, this, &MainWindow::processDiff);
 
     connect(m_copyBtn, &QPushButton::clicked,
             this, [=] (bool checked) {
@@ -122,6 +140,16 @@ MainWindow::MainWindow(QWidget *parent)
 
         qDebug()<<Q_FUNC_INFO<<"duption option "<<m_dupOpt;
 
+        for (const auto [k,v] : m_srcFileMap.asKeyValueRange()) {
+            for (const auto & it : std::as_const(v)) {
+                QString msg(tr("copy"));
+                msg.append(" ");
+                msg.append(k);
+                msg.append("/");
+                msg.append(it.value("name").toString());
+                toLogView(msg);
+            }
+        }
     });
 
     connect(m_dupOptGroup, &QButtonGroup::idClicked,
@@ -134,19 +162,121 @@ MainWindow::MainWindow(QWidget *parent)
 
 MainWindow::~MainWindow()
 {
+    //TODO delete replies
+}
+
+void MainWindow::processDiff()
+{
+    if (!chkServerAndToken()) {
+        return;
+    }
+    m_srcDirList.append(CopyObject(m_srcPathEdt->text(), m_dstPathEdt->text()));
+    m_dstDirList.append(CopyObject(m_srcPathEdt->text(), m_dstPathEdt->text()));
+
+    processSrcDir();
+    processDstDir();
+}
+
+void MainWindow::toLogView(const QString &log) const
+{
+    if (log.isEmpty()) {
+        return;
+    }
+    const auto timeStamp = QDateTime::currentDateTime(QTimeZone::LocalTime).toString("yyyy.MM.dd-hh:mm:ss.zzz");
+    m_logWidget->addItem(QString("%1: %2").arg(timeStamp, log));
+    m_logWidget->scrollToBottom();
 }
 
 bool MainWindow::chkServerAndToken() const
 {
     if (m_server.isEmpty()) {
-        m_logWidget->insertItem(0, tr("Server address is empty!!"));
+        toLogView(tr("Server address is empty!!"));
         return false;
     }
     if (m_token.isEmpty()) {
-        m_logWidget->insertItem(0, tr("Token is empty!!"));
+        toLogView(tr("Token is empty!!"));
         return false;
     }
-    m_logWidget->insertItem(0, QString("Server: %1, Token: %2").arg(m_server, m_token));
+    toLogView(QString("Server: %1").arg(m_server));
+    toLogView(QString("Token: %2").arg(m_token));
     return true;
 }
+
+void MainWindow::processSrcDir()
+{
+    if (m_srcDirList.isEmpty()) {
+        return;
+    }
+
+    QNetworkRequest req(m_server + QLatin1String("/api/fs/list"));
+    req.setRawHeader("Authorization",   m_token.toUtf8());
+    req.setRawHeader("Content-Type",    "application/json");
+
+    auto srcDirObj = m_srcDirList.takeFirst();
+    QVariantMap map;
+    map.insert("path", srcDirObj.srcDir());
+
+    auto reply = m_networkMgr->post(req, QJsonDocument::fromVariant(map).toJson());
+
+    connect(reply, &QNetworkReply::finished,
+            this, [=] {
+        QJsonParseError error;
+        QJsonDocument doc = QJsonDocument::fromJson(reply->readAll(), &error);
+        reply->deleteLater();
+        if (error.error != QJsonParseError::NoError) {
+            toLogView("Parse reply data error for request [" + req.url().toString() + "]");
+            return;
+        }
+        if (!doc.isObject()) {
+            toLogView("Reply data is not json object for request [" + req.url().toString() + "]");
+            return;
+        }
+
+        auto root = doc.object();
+        int code = root.value("code").toInt();
+        if (code != 200) {
+            toLogView("Return code is not 200 for request [" + req.url().toString() + "]");
+            return;
+        }
+
+        auto array = root.value("data").toObject().value("content").toArray();
+
+        qDebug()<<">>>> srcDirObj is "<<srcDirObj.srcDir();
+
+        for (const auto &it : std::as_const(array)) {
+            // qDebug()<<it;
+
+            if (auto o = it.toObject(); !o.isEmpty()) {
+                auto isDir      = o.value("is_dir").toBool();
+                auto name       = o.value("name").toString();
+                auto fullPath   = QString("%1/%2").arg(srcDirObj.srcDir(), name);
+                if (isDir) {
+                    m_srcDirList.append(CopyObject(fullPath, srcDirObj.dstDir()));
+                } else {
+                    auto list = m_srcFileMap.value(fullPath);
+                    list.append(o);
+                    m_srcFileMap.insert(fullPath, list);
+                }
+            }
+        }
+        processSrcDir();
+    });
+}
+
+void MainWindow::processDstDir()
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
--- MainWindow.h
+++ MainWindow.h
@@ -1,12 +1,102 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QSharedData>
+#include <QSharedDataPointer>
+#include <QList>
+#include <QJsonObject>
+
 #include <QMainWindow>
 #include <QLineEdit>
 #include <QLabel>
 #include <QPushButton>
 #include <QButtonGroup>
 #include <QListWidget>
+
+#include <QNetworkAccessManager>
+
+
+class CopyObjectData : public QSharedData
+{
+public:
+    CopyObjectData()
+    {}
+
+    QString srcDir;
+    QString dstDir;
+    QList<QJsonObject> fileList;
+};
+
+class CopyObject
+{
+public:
+    CopyObject()
+    {
+        d = new CopyObjectData;
+    }
+    CopyObject(const QString &srcDir, const QString &dstDir, const QList<QJsonObject> &fileList = QList<QJsonObject>())
+    {
+        d = new CopyObjectData;
+        d->srcDir = srcDir;
+        d->dstDir = dstDir;
+        d->fileList = fileList;
+    }
+    CopyObject(const CopyObject &other)
+        : d(other.d)
+    {
+
+    }
+    bool operator==(const CopyObject &other) const
+    {
+        return d->srcDir        == other.d->srcDir
+                && d->dstDir    == other.d->dstDir
+                && d->fileList  == other.d->fileList;
+    }
+    bool operator != (const CopyObject &other) const
+    {
+        return !operator ==(other);
+    }
+    CopyObject &operator = (const CopyObject &other)
+    {
+        if (this != &other)
+             d.operator =(other.d);
+         return *this;
+    }
+
+    QString srcDir() const
+    {
+        return d->srcDir;
+    }
+    void setSrcDir(const QString &newSrcDir)
+    {
+        d->srcDir = newSrcDir;
+    }
+
+    QString dstDir() const
+    {
+        return d->srcDir;
+    }
+    void setDstDir(const QString &newDstDir)
+    {
+        d->srcDir = newDstDir;
+    }
+    QList<QJsonObject> fileList() const
+    {
+        return d->fileList;
+    }
+    void setFileList(const QList<QJsonObject> &newFileList)
+    {
+        d->fileList = newFileList;
+    }
+    void appendFile(const QJsonObject &obj)
+    {
+        if (!d->fileList.contains(obj)) {
+            d->fileList.append(obj);
+        }
+    }
+private:
+     QSharedDataPointer<CopyObjectData> d;
+};
 
 
 
@@ -23,10 +113,17 @@ public:
         OverwriteWhenNewModified, // overwrite when new modified data of src file
         OverwriteWhenNewCreated,  // overwrite when new created data of src file
         OverwriteUnconditionally
+        //TODO compare hash info
     };
+
+    void processDiff();
+    void toLogView(const QString &log) const;
 
 private:
     bool chkServerAndToken() const;
+    void processSrcDir();
+    void processDstDir();
+
 
 
 private:
@@ -39,15 +136,18 @@ private:
     QPushButton *m_showDiffBtn;
     QListWidget *m_logWidget;
 
+    QNetworkAccessManager *m_networkMgr;
+
     DuplicateOption m_dupOpt;
 
     QString m_server;
     QString m_token;
 
-    QStringList m_srcFileList;
-    QStringList m_dstFileList;
-    QStringList m_srcDirList;
-    QStringList m_dstDirList;
+    // QList<CopyObject> m_srcFileList;
+    // QList<CopyObject> m_dstFileList;
+    QMap<QString, QList<QJsonObject>> m_srcFileMap;
+    QList<CopyObject> m_srcDirList;
+    QList<CopyObject> m_dstDirList;
 
 };
 #endif // MAINWINDOW_H
