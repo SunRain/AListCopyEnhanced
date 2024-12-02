1b0a17fbe7858ab44259335316c57791de29742f
SunRain <41245110@qq.com>
2024-12-02 16:48:40 +0800

    add private const values for my server test

---
+++ PrivateConst.h
@@ -0,0 +1,13 @@
+#ifndef PRIVATECONST_H
+#define PRIVATECONST_H
+
+#include <QString>
+
+
+const QString P_SERVER;
+const QString P_TOKEN;
+const QString P_SRC_DIR;
+const QString P_DST_DIR;
+
+
+#endif // PRIVATECONST_H
